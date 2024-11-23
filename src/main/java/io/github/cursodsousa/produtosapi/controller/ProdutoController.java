32e388f2eba4356e164b63cd67769296d5e84603
WilliansMercante <willians_mercante@hotmail.com>
2024-11-23 20:07:02 -0300

    Implementando os metodos para crud produtos

--- src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -1,15 +1,37 @@
 package io.github.cursodsousa.produtosapi.controller;
 
 import io.github.cursodsousa.produtosapi.model.Produto;
+import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
-    public Produto salvar(@RequestBody Produto produto){
+    public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto recebido: " + produto);
+        produto.setId(UUID.randomUUID().toString());
+        produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id) {
+         produtoRepository.deleteById(id);
     }
 }
--- src/main/java/io/github/cursodsousa/produtosapi/model/Produto.java
+++ src/main/java/io/github/cursodsousa/produtosapi/model/Produto.java
@@ -1,11 +1,28 @@
 package io.github.cursodsousa.produtosapi.model;
 
 // POJO -> Plain Old Java Object
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Produto")
 public class Produto {
 
+    //@column necesssário somente de nomes diferente das tabelas do banco
+    @Id
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco")
     private Double preco;
 
     public String getId() {
